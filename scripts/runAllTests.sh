301ee43d3817560b2b45eb5df19ab2dbe2012c6a
ryanbarrett1515 <barrettra@g.cofc.edu>
2019-10-08 11:47:07 -0400

    updated script and template

--- scripts/runAllTests.sh
+++ scripts/runAllTests.sh
@@ -2,12 +2,12 @@
 cd "`dirname \"$0\"`/"..
 javac project/*.java
 
-for fileName in testCases/*.txt; do
-	echo $fileName
-	for ((i = 0; i < 8; i++)) do
+for fileName in testCases/testCase*.txt; do
+	testID=`sed '3q;d' $fileName`
+	requirement=`sed '4q;d' $fileName`
+	component=`sed '5q;d' $fileName`
+	method=`sed '6q;d' $fileName`
+	param1=`sed '7q;d' $fileName`
+	param2=`sed '8q;d' $fileName`
+	oracle=`sed '9q;d' $fileName`
 done
-
-
-
-output=$(java project.GCDDriver)
-
