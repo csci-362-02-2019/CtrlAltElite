3277d445d17cd4d1ac4ac1c179aa8e0afb7d4cc0
Tyler <malonet@g.cofc.edu>
2019-11-13 15:16:25 -0500

    deleted irrelevant files

--- myList.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONTAININGDIRECTORY="`dirname \"$0\"`/"
-FILE="./currDirectory.html"
-rm -f $FILE
-DIRECTORYCONTENTS="$(ls $CONTAININGDIRECTORY)"
-echo "<html>$DIRECTORYCONTENTS</html>" >> $FILE
-xdg-open $FILE
